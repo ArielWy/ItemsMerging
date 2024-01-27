abce0d4e223d6ccc5bfe80827aa9a93ceacb848b
Ariel <127093985+ariel3007@users.noreply.github.com>
2024-01-27 23:45:12 +0200

    initial commit - convert to kotlin

---
+++ src/main/java/me/_olios/itemsmerging/ItemsMerging.kt
@@ -0,0 +1,13 @@
+package me._olios.itemsmerging
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ItemsMerging : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
